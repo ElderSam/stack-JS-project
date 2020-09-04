18ca9c119458ea71030c5be92966f4ea12c71109
ElderSam <eldersamuel98@gmail.com>
2020-09-04 15:29:57 -0300

    criando primeira rota

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@ const express = require('express'); //adiciona o express
 
 app = express(); 
 
+app.get('/', (req, res) => {
+    res.send('Hello World! ;)');
+});
+
 app.listen(3001); /* inicia o servidor e 'ouve' a porta 3001 do nosso navegador */
