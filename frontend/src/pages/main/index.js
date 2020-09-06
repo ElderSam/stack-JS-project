c0f8bb72da4f2872cd74f2d5611de04567a1b2cd
ElderSam <eldersamuel98@gmail.com>
2020-09-06 13:39:05 -0300

    armazenando no estado

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -3,6 +3,10 @@ import api from '../../services/api';
 
 export default class Main extends Component {
 
+    state = {
+        products: []
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
@@ -10,10 +14,18 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get("/products")
         
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs })
+        console.log(this.state.products);
     }
 
     render() {
-        return <h1>Hello People!</h1>
+        //return <h1>Contagem de produtos: {this.state.products.length}</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        );
     }
 }
